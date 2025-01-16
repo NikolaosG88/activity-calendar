8b192f18a6c4e62de23900ce2757f311d9d26279
NikolaosG <Nikolaos_Gkazgkas@outlook.com>
2025-01-16 00:37:04 +0000

    Activity-Calendar

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-app.use(cors());
+app.use(cors({ origin: 'https://activity-program.netlify.app/' }));
 app.use(express.json());
 
 // Routes go here
